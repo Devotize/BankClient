5c4274e5b8a2686ba93fd8e52bc16fca03fa0c0f
Denis Sychev <sychev@peterpartner.net>
2023-05-23 09:52:06 +0300

    make main view model global singleton

--- iosApp/bankClientiOS/ui/main_view/MainViewModel.swift
+++ iosApp/bankClientiOS/ui/main_view/MainViewModel.swift
@@ -13,6 +13,27 @@ class MainViewModel: ObservableObject {
     private let userRepository = repositoriesProvider.bankUserRepository
     private let currencyRepository = repositoriesProvider.currencyRepository
     
+    private init() {
+        fetchUsers { users in
+            DispatchQueue.main.async {
+                self.usersData = users
+                self.selectedUser = users?.users.first
+            }
+        }
+        fetchCurrency { currency in
+            DispatchQueue.main.async {
+                self.currency = currency
+                self.selectedCurrency = currency?.valute.gBP
+            }
+        }
+    }
+
+    private static var instance = { MainViewModel() }()
+    
+    static func getInstance() -> MainViewModel {
+        return instance
+    }
+    
     @Published var usersData: Users? = nil
     
     @Published var selectedCurrency: CurrencyItem? = nil
@@ -30,21 +51,6 @@ class MainViewModel: ObservableObject {
     func fetchCurrency(callback: @escaping (Currency?) -> Void) {
         currencyRepository.getCurrency { currency, error in
             callback(currency)
-        }
-    }
-    
-    init() {
-        fetchUsers { users in
-            DispatchQueue.main.async {
-                self.usersData = users
-                self.selectedUser = users?.users.first
-            }
-        }
-        fetchCurrency { currency in
-            DispatchQueue.main.async {
-                self.currency = currency
-                self.selectedCurrency = currency?.valute.gBP
-            }
         }
     }
     
