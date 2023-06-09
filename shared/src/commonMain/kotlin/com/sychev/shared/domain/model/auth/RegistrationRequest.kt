8eb11d9c743fe7ce6a51e11e0a9c1806325ddc56
Denis Sychev <sychev@peterpartner.net>
2023-06-09 22:13:13 +0300

    add network config

--- shared/src/commonMain/kotlin/com/sychev/shared/domain/model/auth/RegistrationRequest.kt
+++ shared/src/commonMain/kotlin/com/sychev/shared/domain/model/auth/RegistrationRequest.kt
@@ -1,5 +1,8 @@
 package com.sychev.shared.domain.model.auth
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class RegistrationRequest(
     val email: String,
     val password: String,
